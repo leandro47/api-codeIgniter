2d9118140aa84e7039ea619851d1986a4cc9921b
leandro47 <leandrosilva47@live.com>
2021-04-25 15:08:15 -0300

    update cv

--- app/Controllers/ResumeController.php
+++ app/Controllers/ResumeController.php
@@ -6,7 +6,6 @@ class ResumeController extends BaseController
 {
     public function getResume()
     {
-    
-        return $this->response->download('assets/docs/resume.docx', null)->setFileName('curriculo.docx');
+        return $this->response->download('assets/docs/resume.pdf', null)->setFileName('curriculo.pdf');
     }
 }
